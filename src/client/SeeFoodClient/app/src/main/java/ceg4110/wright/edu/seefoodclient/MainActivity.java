41e78bd4734ab9472007269c00b488dcd9afee08
DMiller <miller.189@wright.edu>
2017-11-07 12:43:17 -0500

    Basic layout for main activity

--- src/client/SeeFoodClient/app/src/main/java/ceg4110/wright/edu/seefoodclient/MainActivity.java
+++ src/client/SeeFoodClient/app/src/main/java/ceg4110/wright/edu/seefoodclient/MainActivity.java
@@ -1,13 +1,28 @@
 package ceg4110.wright.edu.seefoodclient;
 
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager pager;
+    PagerAdapter adapter;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
