856ddd223bdc57b4682aa7eee7623d0f4f4dd7cd
DMiller <miller.189@wright.edu>
2017-11-16 12:40:32 -0500

    Expanded camera functionality

---
+++ src/client/SeeFoodClient/app/src/main/java/ceg4110/wright/edu/seefoodclient/ImageAdapter.java
@@ -0,0 +1,49 @@
+package ceg4110.wright.edu.seefoodclient;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by DJ on 11/16/2017.
+ */
+
+public class ImageAdapter extends PagerAdapter {
+    Context context;
+    private int[] GalImages = new int[] {
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three
+    };
+    ImageAdapter(Context context){
+        this.context=context;
+    }
+    @Override
+    public int getCount() {
+        return GalImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        //int padding = context.getResources().getDimensionPixelSize(R.dimen.app_icon_size);
+        //imageView.setPadding(padding, padding, padding, padding);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageResource(GalImages[position]);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+}
--- src/client/SeeFoodClient/app/src/main/java/ceg4110/wright/edu/seefoodclient/MainActivity.java
+++ src/client/SeeFoodClient/app/src/main/java/ceg4110/wright/edu/seefoodclient/MainActivity.java
@@ -1,19 +1,32 @@
 package ceg4110.wright.edu.seefoodclient;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     PagerAdapter adapter;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ViewPager pager = (ViewPager)findViewById(R.id.viewPager);
-
+        ImageAdapter adapter = new ImageAdapter(this);
+        pager.setAdapter(adapter);
 
         Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
         //create a list of items for the spinner.
@@ -30,10 +44,55 @@ public class MainActivity extends AppCompatActivity {
         dropdown.setAdapter(spinAdapter);
     }
 
-    public void dispatchTakePictureIntent(View view) {
+    public void dispatchTakePictureIntent(View v) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Always use this format for error handling
+                messageBox("IOException", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "ceg4110.wright.edu.seefoodclient.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    // Message dialog for exception handling
+    private void messageBox(String method, String message)
+    {
+        Log.d("EXCEPTION: " + method,  message);
+
+        AlertDialog.Builder messageBox = new AlertDialog.Builder(this);
+        messageBox.setTitle(method);
+        messageBox.setMessage(message);
+        messageBox.setCancelable(false);
+        messageBox.setNeutralButton("OK", null);
+        messageBox.show();
     }
 }
